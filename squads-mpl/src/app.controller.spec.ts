718ddd54092e8c53d4ba19fd1d82cd34fbcb20e5
Isaac Yong <isaac@cakedefi.com>
2023-08-28 17:48:55 +0800

    Update test

--- squads-mpl/src/app.controller.spec.ts
+++ squads-mpl/src/app.controller.spec.ts
@@ -57,7 +57,24 @@ describe('AppController', () => {
         "test multisig",
       );
 
-      console.log(multiSigAccount);
+      // seems like regenerating wallets for aidrops is limited (error 429).
+      // recommend to use the same wallet for testing (generate a keypair on your own and use that (modify the code) instead of regenerating).
+      expect(multiSigAccount).toStrictEqual({
+        threshold: 2,
+        authorityIndex: 1,
+        transactionIndex: expect.any(Number),
+        msChangeIndex: expect.any(Number),
+        bump: expect.any(Number),
+        createKey: squads.wallet.publicKey,
+        allowExternalExecute: false,
+        keys: expect.arrayContaining([
+          member1.publicKey,
+          member2.publicKey,
+          member3.publicKey,
+          squads.wallet.publicKey,
+        ]),
+        publicKey: expect.any(PublicKey),
+      })
     }, 100000 );
   });
 });
